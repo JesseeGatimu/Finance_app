da24e6adc76076398212089469910eef052ab941
Jessee Gatimu <gakuojessee@gmail.com>
2026-04-25 11:20:54 +0300

    Initial commit

---
+++ backend/accounts/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+class User(AbstractUser):
+    email=models.EmailField(unique=True,blank=False,null=False)
+    def __str__(self):
+        return self.username
